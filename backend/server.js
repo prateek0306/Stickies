254f6810ea8bc48694fb4d086f8efaa9a5d1f6f9
Prateek <prateekbaranwal365@gmail.com>
2026-04-23 12:43:38 +0530

    added json files and updated server.js to use environment variables for MongoDB connection.

--- backend/server.js
+++ backend/server.js
@@ -40,4 +40,5 @@ app.delete("/notes/:id", async (req, res) => {
   res.send("Deleted");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
